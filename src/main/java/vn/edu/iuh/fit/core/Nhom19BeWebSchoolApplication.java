e668c7bbd089561f02898d7ec401cacb773b3c5e
My Chau <nguyenlemychau1201@gmail.com>
2024-05-09 22:45:58 +0700

    Create project BE

---
+++ src/main/java/vn/edu/iuh/fit/core/Nhom19BeWebSchoolApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nhom19BeWebSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nhom19BeWebSchoolApplication.class, args);
+    }
+
+}
