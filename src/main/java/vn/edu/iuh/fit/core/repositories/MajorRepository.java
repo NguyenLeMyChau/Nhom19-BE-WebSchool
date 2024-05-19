7693751417948dfaa121dac03f226d7e5bb54984
Nguyễn Lê Mỹ Châu <144518175+NguyenLeMyChau@users.noreply.github.com>
2024-05-19 23:36:26 +0700

    Delete src directory

--- src/main/java/vn/edu/iuh/fit/core/repositories/MajorRepository.java
+++
@@ -1,7 +0,0 @@
-package vn.edu.iuh.fit.core.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vn.edu.iuh.fit.core.models.Major;
-
-public interface MajorRepository extends JpaRepository<Major, Integer> {
-}
--- src/main/java/vn/edu/iuh/fit/core/repositories/SemesterRepository.java
+++
@@ -1,7 +0,0 @@
-package vn.edu.iuh.fit.core.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vn.edu.iuh.fit.core.models.Semester;
-
-public interface SemesterRepository extends JpaRepository<Semester, Integer> {
-}
